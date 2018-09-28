da27741f1c099d8b7c41f34a5674dca246e8ece9
Paul Lave <pmlave@gmail.com>
2018-09-28 05:14:13 +0000

    Solution for nop opcode

---
+++ _nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * _nop - do nothing
+ * @stack: Unused in this context
+ * @line_number: Unused in this context
+ * Return: Void
+ */
+
+void _nop(au stack_t **stack, au unsigned int line_number)
+{
+	return;
+}
--- get_func.c
+++ get_func.c
@@ -12,6 +12,7 @@ int (*get_func(void))(stack_t **stack, unsigned int line_number)
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
+		{"nop", _nop},
 		{NULL, NULL}
 	};
 	int i = 0;
